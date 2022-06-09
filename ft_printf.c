7a739131691a2c21ace960781265676d7cc54b88
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-06-09 13:36:20 +0200

    Ckpt

--- ft_printf.c
+++ ft_printf.c
@@ -6,31 +6,31 @@
 /*   By: segarcia <segarcia@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 11:14:54 by segarcia          #+#    #+#             */
-/*   Updated: 2022/06/09 12:52:05 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/06/09 13:23:32 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_handler(va_list args, const char format)
+static int	ft_print_handler(va_list args, const char format)
 {
 	int	print_len;
 
 	print_len = 0;
-	if (format == '%')
-		print_len += print_char('%');
 	if (format == 'c')
 		print_len += print_char(va_arg(args, int));
 	if (format == 's')
 		print_len += print_str(va_arg(args, char *));
-	if (format == 'i' || format == 'd')
+	if (format == 'p')
+		print_len += ft_printf_ptr(va_arg(args, unsigned long long));
+	if (format == 'd' || format == 'i')
 		print_len += ft_printf_int(va_arg(args, int));
 	if (format == 'u')
 		print_len += ft_printf_uint(va_arg(args, unsigned int));
 	if (format == 'x' || format == 'X')
 		print_len += ft_printf_hex(va_arg(args, unsigned int), format);
-	if (format == 'p')
-		print_len += ft_printf_ptr(va_arg(args, unsigned long long));
+	if (format == '%')
+		print_len += print_char('%');
 	return (print_len);
 }
 
@@ -47,7 +47,7 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%')
 		{
-			print_len += ft_handler(arg, str[i + 1]);
+			print_len += ft_print_handler(arg, str[i + 1]);
 			i++;
 		}
 		else
